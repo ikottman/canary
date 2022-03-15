db7f8434028e40b0bb7400442d6aa8a2e2edc16f
Ian Kottman <ikottman@users.noreply.github.com>
2022-03-14 19:42:53 -0500

    update measurement from request body

--- src/app.go
+++ src/app.go
@@ -3,9 +3,11 @@ package main
 import (
 	"database/sql"
 	"embed"
+	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +20,17 @@ var resources embed.FS
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
 var db, err = sql.Open("sqlite3", "measurements.db")
+
+type Measurement struct {
+	Temperature   float32
+	Pressure      float32
+	Humidity      float32
+	GasResistance int
+	IAQ           float32
+	Accuracy      int
+	CO2           float32
+	VOC           float32
+}
 
 func checkErr(err error) {
 	if err != nil {
@@ -56,7 +69,8 @@ func readFromDatabase() {
 	rows.Close()
 }
 
-func recordMeasurement() {
+func recordMeasurement(measurement Measurement) {
+
 	const query = `
 		INSERT INTO measurements(
 			temperature,
@@ -74,14 +88,14 @@ func recordMeasurement() {
 	statement, err := db.Prepare(query)
 	checkErr(err)
 	var _, error = statement.Exec(
-		24.65,
-		1019.42,
-		46.23,
-		597617,
-		26.9,
-		2,
-		506.7,
-		0.51,
+		measurement.Temperature,
+		measurement.Pressure,
+		measurement.Humidity,
+		measurement.GasResistance,
+		measurement.IAQ,
+		measurement.Accuracy,
+		measurement.CO2,
+		measurement.VOC,
 		time.Now().Format("2006-01-02T15:04:05Z"),
 	)
 	checkErr(error)
@@ -104,7 +118,17 @@ func main() {
 	})
 
 	http.HandleFunc("/measurement", func(w http.ResponseWriter, r *http.Request) {
-		recordMeasurement()
+		requestBody, _ := ioutil.ReadAll(r.Body)
+		var measurement Measurement
+		json.Unmarshal(requestBody, &measurement)
+		recordMeasurement(measurement)
+	})
+
+	http.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		stmt, err := db.Prepare("delete from measurements where 1=1")
+		checkErr(err)
+
+		stmt.Exec()
 	})
 
 	log.Println("listening on", port)
